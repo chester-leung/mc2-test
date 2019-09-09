18ec2f999426367275f449375bc23478873c4053
chester-leung <root@ip-172-31-7-72.us-west-2.compute.internal>
2019-09-09 09:19:12 +0000

    no list slicing

--- xgb_standalone/xgb_ssh.py
+++ xgb_standalone/xgb_ssh.py
@@ -2,21 +2,27 @@ import xgboost as xgb
 from numpy import genfromtxt
 
 def main():
-    data = genfromtxt('/home/ubuntu/mc2/data/msd_training_data_split.csv', delimiter=',')
-    labels = data[:, 0]
-    data = data[:, 1:]
-    dtrain = xgb.DMatrix(data, label=labels)
+    data = genfromtxt('/home/ubuntu/mc2/data/msd_training_data_split_sample.csv', delimiter=',')
+    #labels = data[:, 0]
+    #data = data[:, 1:]
+    dtrain = xgb.DMatrix(data[:, 1:], label=data[:, 0])
 
     print("Rank: {}, dtrain rows: {}".format(xgb.rabit.get_rank(), dtrain.num_row()))
     params = {'max_depth': 3, 'min_child_weight': 1.0, 'lambda': 1.0}
-    num_rounds = 100
+    num_rounds = 20
     
     model = xgb.train(params, dtrain, num_rounds)
 
     if xgb.rabit.get_rank() == 0:
-        dtest = xgb.DMatrix('/home/ubuntu/mc2/data/msd_test_data_libsvm.data')
+        #model.save_model('saved.model')
+        #dtest = xgb.DMatrix('/home/ubuntu/mc2/data/msd_test_data_libsvm.data')
+        dtest = xgb.DMatrix('/home/ubuntu/mc2/xgb_standalone/tutorial/msd_test_data_sample.libsvm')
+
+        #test_data = genfromtxt('/home/ubuntu/mc2/data/msd_test_data.csv', delimiter=',')
+        #test_labels = test_data[:, 0]
+        #test_data = test_data[:, 1:]
+        #dtest = xgb.DMatrix(test_data, label=test_labels)
         print(model.eval(dtest))
-        model.save_model('saved.model')
 
 if __name__ == '__main__':
     xgb.rabit.init()
