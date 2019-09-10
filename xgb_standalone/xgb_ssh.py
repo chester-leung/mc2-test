7c757c521c78e7a6a74e30de4318ee185b532100
chester-leung <root@ip-172-31-7-72.us-west-2.compute.internal>
2019-09-10 00:01:12 +0000

    print statements

--- xgb_standalone/xgb_ssh.py
+++ xgb_standalone/xgb_ssh.py
@@ -1,8 +1,11 @@
+print("importing xgb")
 import xgboost as xgb
 from numpy import genfromtxt
 
 def main():
+    print("In start of main")
     data = genfromtxt('/home/ubuntu/mc2/data/msd_training_data_split.csv', delimiter=',')
+    print("data genfromtxt")
     #labels = data[:, 0]
     #data = data[:, 1:]
     dtrain = xgb.DMatrix(data[:, 1:], label=data[:, 0])
@@ -11,8 +14,9 @@ def main():
     params = {'max_depth': 3, 'min_child_weight': 1.0, 'lambda': 1.0}
     num_rounds = 20
     
+    print("do i make it here")
     model = xgb.train(params, dtrain, num_rounds)
-
+    print("done training")
     if xgb.rabit.get_rank() == 0:
         #model.save_model('saved.model')
         #dtest = xgb.DMatrix('/home/ubuntu/mc2/data/msd_test_data_libsvm.data')
@@ -25,13 +29,17 @@ def main():
         print(model.eval(dtest))
 
 if __name__ == '__main__':
+    print("about to init")
     xgb.rabit.init()
+    print("init rabit")
 
+    print("getting world size")
     n_workers = xgb.rabit.get_world_size()
+    print("getting rank")
     rank = xgb.rabit.get_rank()
-    
+    print("about to main")
     main()
-
+    print("after main")
     xgb.rabit.finalize()
 
     
