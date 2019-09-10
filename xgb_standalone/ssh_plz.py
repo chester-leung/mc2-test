c8b6eb5a0963cc7bf0778ec2876f08bd817cb39f
root <root@ip-172-31-41-140.us-west-2.compute.internal>
2019-09-10 00:39:22 +0000

    use numpy array

--- xgb_standalone/ssh_plz.py
+++ xgb_standalone/ssh_plz.py
@@ -2,7 +2,9 @@ import xgboost as xgb
 from numpy import genfromtxt
 
 def main():
-    dtrain = xgb.DMatrix('/home/ubuntu/mc2/xgb_standalone/tutorial/msd_training_data_sample.libsvm')
+    #dtrain = xgb.DMatrix('/home/ubuntu/mc2/xgb_standalone/tutorial/msd_training_data_sample.libsvm')
+    data = genfromtxt("/home/ubuntu/mc2/data/msd_training_data_split.csv", delimiter=',')
+    dtrain = xgb.DMatrix(data[:, 1:], label=data[:, 0])
     dtest = xgb.DMatrix('/home/ubuntu/mc2/xgb_standalone/tutorial/msd_test_data_sample.libsvm')
 
     params = {'max_depth': 3, 'min_child_weight': 1.0, 'lambda': 1.0}
